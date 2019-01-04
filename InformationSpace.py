b217a519ecf9e3fd50b5e4eda15463db66417e19
sergiopaniego <sergiopaniegoblanco@gmail.com>
2019-01-04 15:05:42 +0100

    Extract information from file

--- InformationSpace.py
+++ InformationSpace.py
@@ -23,6 +23,14 @@ revision_date = ''
 expiration_date = ''
 conservation_protocol = ''
 conduction_and_machinery_use = ''
+kids_administration_method = ''
+elderly_administration_method = ''
+overdose_protocol = ''
+forgot_medication = ''
+adverse_effects = ''
+adverse_effects_communication = ''
+dosis = ''
+prohibitions = ''
 
 
 def extract_composition(file):
@@ -65,6 +73,7 @@ def extract_purpose(file):
     while len(line.split('2. ')) <= 1:
         purpose += line
         line = file.readline()
+    return line
 
 
 def extract_expiration_date(file, line):
@@ -89,7 +98,83 @@ def extract_conduction_and_machinery_use(file):
         line = file.readline()
 
 
+def extract_kids_administration_method(file):
+    global kids_administration_method
+    line = file.readline()
+    while len(line.split('.')) == 1:
+        kids_administration_method += line
+        line = file.readline()
+    kids_administration_method += line
+
+
+def extract_elderly_administration_method(file):
+    global elderly_administration_method
+    line = file.readline()
+    while len(line.split('.')) == 1:
+        elderly_administration_method += line
+        line = file.readline()
+    if line.split('.')[1] != '\n':
+        elderly_administration_method += line
+        line = file.readline()
+    elderly_administration_method += line
+
+
+def extract_overdose_protocol(file):
+    global overdose_protocol
+    line = file.readline()
+    while len(line.split('Si olvidó tomar')) == 1:
+        overdose_protocol += line
+        line = file.readline()
+    return line
+
+
+def extract_forgot_medication(file):
+    global forgot_medication
+    line = file.readline()
+    while len(line.split('4.')) == 1:
+        forgot_medication += line
+        line = file.readline()
+    return line
+
+
+def extract_adverse_effects(file):
+    global adverse_effects
+    line = file.readline()
+    while len(line.split('Comunicación de efectos adversos')) == 1:
+        adverse_effects += line
+        line = file.readline()
+    return line
+
+
+def extract_adverse_effects_communication(file):
+    global adverse_effects_communication
+    line = file.readline()
+    while len(line.split('5. Conservación de')) == 1:
+        adverse_effects_communication += line
+        line = file.readline()
+    return line
+
+
+def extract_dosis(file, line):
+    global dosis
+    if line.split(':')[1] != '\n':
+        dosis += line
+    line = file.readline()
+    dosis += line
+
+
+def extract_prohibitions(file):
+    global prohibitions
+    line = file.readline()
+    while len(line.split('Uso de')) == 1 or len(line.split('otros medicamentos')) == 1:
+        prohibitions += line
+        line = file.readline()
+    return line
+
+
 count_1 = 0
+count_2 = 0
+count_4 = 0
 count_5 = 0
 while line:
     if len(line.split('Composición')) > 1:
@@ -112,12 +197,8 @@ while line:
         if count_1 == 0:
             count_1 += 1
         else:
-            extract_purpose(file)
+            line = extract_purpose(file)
             print('Purpose [\n' + purpose + ']')
-        line = file.readline()
-    elif len(line.split('2. ')) > 1:
-        # print(line)
-        line = file.readline()
     elif len(line.split('Este prospecto ha sido')) > 1:
         revision_date = line
         print('Revision date [\n' + revision_date + ']')
@@ -138,5 +219,40 @@ while line:
         extract_conduction_and_machinery_use(file)
         print('Conduction and Machinery use [' + conduction_and_machinery_use + ']')
         line = file.readline()
+    elif len(line.split('Uso en niños:')) > 1:
+        extract_kids_administration_method(file)
+        print('Kids administration [' + kids_administration_method + ']')
+        line = file.readline()
+    elif len(line.split('Pacientes de edad avanzada')) > 1 or len(line.split('Uso en mayores de 65 años')) > 1:
+        extract_elderly_administration_method(file)
+        print('Elderly people administration [' + elderly_administration_method + ']')
+        line = file.readline()
+    elif len(line.split('toma más')) > 1:
+        line = extract_overdose_protocol(file)
+        print('Overdose protocol [' + overdose_protocol + ']')
+    elif len(line.split('Si olvidó tomar')) > 1:
+        line = extract_forgot_medication(file)
+        print('Forgot medication [' + forgot_medication + ']')
+    elif len(line.split('4. Posibles efectos adversos')) > 1:
+        if count_4 == 0:
+            count_4 += 1
+            line = file.readline()
+        else:
+            line = extract_adverse_effects(file)
+            print('Adverse effects [' + adverse_effects + ']')
+    elif len(line.split('Comunicación de efectos adversos')) > 1:
+        line = extract_adverse_effects_communication(file)
+        print('Adverse effects communication [' + adverse_effects_communication + ']')
+    elif len(line.split('Adultos y')) > 1:
+        extract_dosis(file, line)
+        print('Dosis [' + dosis + ']')
+        line = file.readline()
+    elif len(line.split('2.')) > 1:
+        if count_2 == 0:
+            count_2 += 1
+            line = file.readline()
+        else:
+            line = extract_prohibitions(file)
+            print('Prohibitions [' + prohibitions + ']')
     else:
         line = file.readline()
